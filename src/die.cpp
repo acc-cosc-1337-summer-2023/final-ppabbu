4637c731bc2c298b2c7d4b01949eda7a5400c8ba
ppabbu <135080457+ppabbu@users.noreply.github.com>
2023-07-28 02:19:50 +0000

    did question 1

--- src/die.cpp
+++ src/die.cpp
@@ -1,1 +1,14 @@
 //
+#include "die.h"
+#include <cstdlib>
+
+
+void Die::roll()
+{
+    roll_value = rand() % sides + 1;
+}
+
+int Die::rolled_value() const
+{
+    return roll_value;
+}
--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -1,6 +1,23 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "die.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+TEST_CASE("Assert die rolls return 1-6") {
+	// require die rolls to return 1-6;
+}
+
+TEST_CASE("Roll 10 times and require rolls to be between 1-6") {
+
+	Die die;
+	for(int i = 0; i < 10; i++)
+	{
+		die.roll();
+		int rollvalue = die.rolled_value();
+		REQUIRE(rollvalue > 0);
+		REQUIRE(rollvalue <= 6);
+	}
 }
